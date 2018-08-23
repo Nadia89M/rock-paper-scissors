c2817b61f1c44378416cfa93adc77e7239a169e1
Nadia <nadia.mohamed89@gmail.com>
2018-08-23 21:52:34 +0200

    Rock Paper Scissors

---
+++ rock-paper-scissors.rb
@@ -0,0 +1,33 @@
+loop do #game loops to keep playing
+  
+  options = ["rock", "paper", "scissors"]
+  computer_choice = options[rand(options.length)]
+  puts "What's your choice?"
+  user_input = gets.chomp.downcase
+
+  while !(user_input == "rock" || user_input == "paper" || user_input == "scissors")
+    puts "Invalid choice, enter Rock, Paper, or Scissors!"
+    puts "What's your choice?"
+    user_input = gets.chomp.downcase
+  end
+  if (user_input == "rock" || user_input == "paper" || user_input == "scissors")
+    if (user_input == computer_choice)
+      puts "We got the same, let's keep playing!"
+    elsif (user_input == "rock" && computer_choice == "scissors")
+      puts "computer choice is: " + computer_choice + " , you win! :)"
+    elsif (user_input == "rock" && computer_choice == "paper")
+      puts "computer choice is: " + computer_choice + " ,computer wins :("
+    elsif (user_input == "paper" && computer_choice == "scissors")
+      puts "computer choice is: " + computer_choice + " ,computer wins :("
+    elsif (user_input == "paper" && computer_choice == "rock")
+      puts "computer choice is: " + computer_choice + " , you win! :)"
+    elsif (user_input == "scissors" && computer_choice == "rock")
+      puts "computer choice is: " + computer_choice + " ,computer wins :("
+    elsif (user_input == "scissors" && computer_choice == "paper")
+      puts "computer choice is: " + computer_choice + " , you win! :)"
+    end
+    puts "Wanna play again? (yes/no)"
+    answer = gets.chomp.downcase
+    break if answer == "no"
+  end
+end
